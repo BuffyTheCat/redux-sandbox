d183d098113cfb6af2e7388cf6294420a857480a
Svyatoslav <buffythecat@yande.ru>
2020-04-26 18:27:43 +0300

    add counter

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
-const initialState = 0;
+import { createStore } from 'redux';
 
-const reduser = (state, action) => {
+const reduser = (state = 0, action) => {
     if (state === undefined) {
         return 0;
     }
     switch (action.type) {
         case 'INC':
             return state + 1;
+        case 'DEC':
+            return state - 1;
         default:
             return state;
     }
 }
 
-let state = reduser(initialState, { type: 'INC'});
-console.log(state);
+const store = createStore(reduser);
+
+document.getElementsByClassName('increment')[0].addEventListener('click', () => {
+    store.dispatch({ type: 'INC'});
+});
+
+
+document.getElementsByClassName('decrement')[0].addEventListener('click', () => {
+    store.dispatch({ type: 'DEC'});
+});
+
+const update = () => {
+    document.getElementsByClassName('counter')[0].innerHTML = store.getState();
+}
+
+store.subscribe(update);
