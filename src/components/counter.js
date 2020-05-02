2bece39e4405a3340f1cf4d27cadc68b23793167
Svyatoslav <buffythecat@yande.ru>
2020-05-02 13:29:16 +0300

    end of work

--- src/components/counter.js
+++ src/components/counter.js
@@ -21,15 +21,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = (dispatch) => {
-    const {inc, dec, rnd } = bindActionCreators(actions, dispatch);
-    return {
-        inc,
-        dec,
-        rnd: () => {
-            const randomVal = Math.floor(Math.random()*10); 
-            rnd(randomVal)
-        }
-    }
+    return bindActionCreators(actions, dispatch)
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
